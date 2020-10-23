11275c8aed9c0ada98cf8124e3c401a1be2d777f
Andrew <kolbe1129@gmail.com>
2020-10-23 03:52:20 -0400

    Second Commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const diceArray = [];
-//Create a class named Die that represents a single die
+const diceArray = []; //placeholder for dice
+
 class Die {
     constructor() {
-        this.value = Math.floor(Math.random() * 7);
+        this.value = Math.floor((Math.random() * 6) + 1); //prevents 0 from occuring
         this.div = $(`<div class="square">${this.value}</div>`);
         $('.square-container').append(this.div);
         this.div.click(() => {
@@ -12,34 +12,25 @@ class Die {
             this.div.remove();
         })
     }
-    //Your class must have a method named roll that generates a random integer 1-6, 
-    //sets the value property, and updates the div on the page with the new value.
-    roll() {
-        console.log('test');
 
+    roll() { //can be used outside of the Die object
+        this.value = Math.floor((Math.random() * 6) + 1);
+        this.div.text(this.value);
     }
 }
 
 
-
-
-//When a new Die is created, it should automatically create a div, 
-// call its roll method, and put the div on the screen
 $('.generate').click(() => {
-
-    let fuckYea = new Die();
-    diceArray.push(fuckYea);
-    console.log(diceArray);
-
-    $('.reroll').click(() => {
-        diceArray.forEach(die => {
-            die.roll();
-        })
-
-    })
+    let diceBoy = new Die();
+    diceArray.push(diceBoy); //pushes new die onto array with every click
 })
-    $('.sum').click(() => {
-        diceArray.reduce().push();
-    })
 
+$('.reroll').click(() => {
+    diceArray.forEach(die => die.roll()); //rolls through every individual die in the array
+})
+
+$('.sum').click(() => {
+    let answer = diceArray.reduce((acc, val) => (acc + val)); // doesn't work with diceArray HALP
+    alert(answer);
+});
 
