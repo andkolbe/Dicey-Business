0ae3bf501339a5bb5f5e6c52cb29fa567283aae3
Andrew <kolbe1129@gmail.com>
2020-10-23 18:30:15 -0400

    Completed with Review

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const diceArray = []; //placeholder for dice
 
 class Die {
+    static collection = []; //placeholder for dice
+    static rollEmUp() {
+        Die.collection.forEach(die => die.roll()); //rolls through every individual die in the array
+    }
+    static addEmUp() {
+        let answer = Die.collection.reduce((total, die) => (total + die.value), 0); // doesn't work with diceArray HALP
+        alert(answer);
+    }
     constructor() {
         this.value = Math.floor((Math.random() * 6) + 1); //prevents 0 from occuring
         this.div = $(`<div class="square">${this.value}</div>`);
@@ -10,27 +17,22 @@ class Die {
         })
         this.div.dblclick(() => {
             this.div.remove();
+            let eachDie = Die.collection.indexOf(this);
+            Die.collection.splice(eachDie, 1);
         })
+        Die.collection.push(this);
     }
 
     roll() { //can be used outside of the Die object
         this.value = Math.floor((Math.random() * 6) + 1);
-        this.div.text(this.value);
+        this.div.text(this.value); //reflects new internal value
     }
 }
 
 
-$('.generate').click(() => {
-    let diceBoy = new Die();
-    diceArray.push(diceBoy); //pushes new die onto array with every click
-})
+$('.generate').click(() => new Die()); //pushes new die onto array with every click
 
-$('.reroll').click(() => {
-    diceArray.forEach(die => die.roll()); //rolls through every individual die in the array
-})
+$('.reroll').click(() => Die.rollEmUp());
 
-$('.sum').click(() => {
-    let answer = diceArray.reduce((acc, val) => (acc + val)); // doesn't work with diceArray HALP
-    alert(answer);
-});
+$('.sum').click(() => Die.addEmUp());
 
